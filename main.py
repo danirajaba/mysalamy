32461e80983654740745fdb58da2d6ae728de45c
danial rajabi <44137507+danirajaba@users.noreply.github.com>
2023-03-30 12:52:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-import itertools
-
 from itertools import product
 from string import ascii_lowercase, digits
 import timeit
 import requests
 import time
-import re
 
-import sys
 
 start = timeit.default_timer()
+date_ = int(time.time())
 
-for item in itertools.product(ascii_lowercase + digits, repeat=9):
-    print('a'+''.join(item))
-    
+
+for item in product(ascii_lowercase + digits, repeat = 9):
+    code = 'a' + ''.join(item)
+
+    url = f""
+    status = requests.get(url).status_code
+
+    print(f"{code}: {status}")
+
+    if status == 200:                
+        with open(f'output_{date_}.txt', 'a') as file:
+            file.write( url + '\n')
+
 stop = timeit.default_timer()
-print(f'Time: ', stop - start)  
+print('Time: ', stop - start)  
